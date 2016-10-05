97e5ae30108195c8ebf5bbe837883ac0f9766bb8
Vadym Kravchenko <linxok@gmail.com>
2016-10-05 16:38:28 +0000

    работает

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -14,9 +14,23 @@
 /**
  * Returns true if value is in array of n values, else false.
  */
-bool search(int value, int values[], int n)
+bool search(int value, int values[], int high)
 {
     // TODO: implement a searching algorithm
+    int middle =0;
+    int low =0;
+    
+    while (low <= high)
+    {
+        middle = (low + high) / 2;
+
+        if (value == values[middle])
+            return true;
+        else if(value < values[middle])
+            high = middle -1;
+        else
+            low = middle +1;
+    }
     return false;
 }
 
@@ -26,5 +40,20 @@ bool search(int value, int values[], int n)
 void sort(int values[], int n)
 {
     // TODO: implement an O(n^2) sorting algorithm
+int temp = 0;  
+bool exit = false; 
+
+while (!exit) // buble sort
+    {
+    exit = true;
+    for (int int_counter = 0; int_counter < (n - 1); int_counter++)  
+        if (values[int_counter] > values[int_counter + 1]) 
+        {
+         temp = values[int_counter];
+         values[int_counter] = values[int_counter + 1];
+         values[int_counter + 1] = temp;
+         exit = false;  
+        }
+    }
     return;
 }
